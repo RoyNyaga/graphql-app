8c4e34b83ee6275961055962aa0fc0d6ab329158
RoyNyaga <nyagaandreroy@gmail.com>
2020-06-01 11:01:52 +0100

    add query in gatsby-node.js file

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,5 +2,27 @@ const path = require('path')
 
 module.exports.createPages = async ({ graphql  actions }) => {
 	const { createPage } = actions
-	const displayTemplate = path.resolve('./src/template/show.js')
+	const displayTemplate = path.resolve('./src/templates/show.js')
+	const res = await graphql(`
+		query{
+	      allGithubData{
+	        nodes{
+	          rawResult{
+	            data{
+	              repositoryOwner{
+	                repositories{
+	                  totalCount
+	                  edges{
+	                    node{
+	                      name                     
+	                    }
+	                  }
+	                }
+	              }
+	            }
+	          }
+	        }
+	      }
+	    }
+		`)
 }
