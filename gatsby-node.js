3f801506586aafe4414b6145bbecf082aacba268
RoyNyaga <nyagaandreroy@gmail.com>
2020-06-02 20:40:02 +0100

    style show page

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,33 +5,40 @@ module.exports.createPages = async ({ graphql, actions }) => {
 	const gitTemplate = path.resolve('./src/templates/show.js')
 	const res = await graphql(`
 		query{
-	      allGithubData{
-	        nodes{
-	          rawResult{
-	            data{
-	              repositoryOwner{
-	                repositories{
-	                  totalCount
-	                  edges{
-	                    node{
-	                      name                     
-	                    }
-	                  }
-	                }
-	              }
-	            }
-	          }
-	        }
-	      }
-	    }
+      allGithubData{
+        nodes{
+          rawResult{
+            data{
+              repositoryOwner{
+                repositories{
+                  totalCount
+                  edges{
+                    node{
+                      name
+                      url
+                      hasIssuesEnabled
+                      createdAt
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 		`)
 
 	res.data.allGithubData.nodes[0].rawResult.data.repositoryOwner.repositories.edges.forEach((edge) => {
 		createPage({
 			component: gitTemplate,
 			path: `/repository/${edge.node.name}`,
-			constext: {
-				name: edge.node.name
+			context: {
+				name: edge.node.name,
+				url: edge.node.url,
+				issues: edge.node.hasIssuesEnabled,
+				createDate: edge.node.createdAt
+
 			}
 		})
 	})
--- src/templates/show.js
+++ src/templates/show.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/githubCard.css'
+import Layout from "../components/layout"
  
 
 
-const Show = () => {
+const Show = (props) => {
+	console.log(props.pageContext)
+	const {name, url, issues, createDate} = props.pageContext
 	return(
-		<div>Hello there</div>
+		<Layout>
+			<div id="github-show-page" className="container">
+				<div id="github-show-page-row-div" className="row">
+					<div className="col-md-5 mx-auto my-auto text-center text-white">
+						<h1>{name}</h1>
+						<p>This Project was created on {createDate}</p>
+						<a href={url}>Follow this link to view it on github</a>
+					</div>
+				</div>
+			</div>
+		</Layout>
 		)
 }
 
-export default Show
+export default Show  
